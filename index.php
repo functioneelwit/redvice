716533a3076f23f35a0e77d4c1d599abc8ad5658
Mattijs Wit <mattijs@functioneelwit.nl>
2024-09-25 11:54:16 +0200

    ..

--- index.php
+++ index.php
@@ -13,3 +13,21 @@
  * @since   Timber 0.1
  */
 
+$context          = Timber::context();
+$context['clients'] = Timber::get_posts([
+    'post_type' => 'client',
+    'posts_per_page' => 6,
+]);
+$context['courses'] = Timber::get_posts([
+    'post_type' => 'course',
+    'posts_per_page' => 6,
+]);
+
+$context['options'] = get_fields('options');
+
+$templates        = array( 'index.twig' );
+if ( is_home() ) {
+	array_unshift($templates, 'home.twig');
+}
+
+Timber::render($templates, $context);
--- single.php
+++ single.php
@@ -9,26 +9,7 @@
  * @since    Timber 0.1
  */
 
-$context         = Timber::context();
-$timber_post     = Timber::get_post();
-$context['post'] = $timber_post;
-
-$acts = Timber::get_posts( [
-    'post_type' => 'act',
-    'edition' => '2024',
-    'posts_per_page' => -1,
-] );
-
-$blacks = [1,2,5,6,9,10,13,14,17,18,21,22,25,26,29,30,33,34];
-
-foreach ($acts as $key => $act) {
-    if($act->ID === $timber_post->ID && in_array($key, $blacks)) {
-        $context['color'] = 'black';
-    }
-}
-
 $context['options'] = get_fields('options');
-
 
 if ( post_password_required( $timber_post->ID ) ) {
 	Timber::render( 'single-password.twig', $context );
